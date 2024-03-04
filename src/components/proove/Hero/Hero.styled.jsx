e27fdaaaa9a74fb9e4e07ae9496a1c265bfe5ff9
Yurii <99128333+CrocodileGenna@users.noreply.github.com>
2024-03-04 17:37:21 +0200

    hero mob

--- src/components/proove/Hero/Hero.styled.jsx
+++ src/components/proove/Hero/Hero.styled.jsx
@@ -1,5 +1,11 @@
 import styled from '@emotion/styled';
+import backgroundImage from './proove_orion_3.png';
 
 export const HERO = styled.div`
   width: 100%;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
+  background-image: url(${backgroundImage});
 `;
